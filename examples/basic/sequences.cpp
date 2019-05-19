019620b6fbf08416184abb56893b6deacc3e8fa2
Florian Tischler <f.tischler@mk-illumination.com>
2019-05-19 20:22:26 +0200

    start sequence building

---
+++ examples/basic/sequences.cpp
@@ -0,0 +1,43 @@
+#include "../../src/sequence.h"
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	int i = 0;
+
+	invoke(hello_world() | hello_world() | []() { cout << "Hello Lambda" << endl; } | task([](/*cgh*/) {
+
+		/*
+
+		auto rw_up = up.template get_access<cl::sycl::access::mode::read_write>(cgh, celerity::access::one_to_one<2>());
+		auto r_u = u.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::neighborhood<2>(1, 1));
+
+		const auto size = up.get_range();
+		cgh.parallel_for<KernelName>(size, [=](cl::sycl::item<2> item) {
+			// NOTE: We have to do some casting due to some weird ComputeCpp behavior - possibly a device compiler bug.
+			// See https://codeplay.atlassian.net/servicedesk/customer/portal/1/CPPB-94 (psalz)
+			const size_t py = item[0] < size[0] - 1 ? item[0] + 1 : item[0];
+			const size_t my = item[0] > 0 ? static_cast<size_t>(static_cast<int>(item[0]) - 1) : item[0];
+			const size_t px = item[1] < size[1] - 1 ? item[1] + 1 : item[1];
+			const size_t mx = item[1] > 0 ? static_cast<size_t>(static_cast<int>(item[1]) - 1) : item[1];
+
+			// NOTE: We have to copy delta here, again to avoid some ComputeCpp weirdness.
+			cl::sycl::float2 delta2 = delta;
+			const float lap = (dt / delta2.y()) * (dt / delta2.y()) * ((r_u[{py, item[1]}] - r_u[item]) - (r_u[item] - r_u[{my, item[1]}]))
+			                  + (dt / delta2.x()) * (dt / delta2.x()) * ((r_u[{item[0], px}] - r_u[item]) - (r_u[item] - r_u[{item[0], mx}]));
+			rw_up[item] = Config::a * 2 * r_u[item] - Config::b * rw_up[item] + Config::c * lap;
+		});
+
+		*/
+	
+	}) | submit_to(distr_queue{}));
+
+	invoke(incr(i) | incr(i));
+
+	cout << i << endl;
+
+	cin.get();
+
+	return i;
+}
---
+++ src/sequence.h
@@ -0,0 +1,86 @@
+#include <tuple>
+#include <functional>
+#include <utility>
+#include <type_traits>
+#include <optional>
+#include <variant>
+#include <iostream>
+
+template<typename... Actions>
+class Sequence
+{
+public:
+	using actions_t = std::tuple<Actions...>;
+
+	Sequence(Actions... actions)
+		: actions_(actions...)
+	{
+
+	}
+
+	void operator()() const
+	{
+		dispatch(std::index_sequence_for<Actions...>{});
+	}
+
+private:
+	actions_t actions_;
+
+	template<size_t...Is>
+	void dispatch(std::index_sequence<Is...>) const
+	{
+		((std::get<Is>(actions_)()), ...);
+	}
+};
+
+template<typename...Actions, typename ActionType>
+Sequence<Sequence<Actions...>, ActionType> operator | (const Sequence<Actions...>& sequence, ActionType action)
+{
+	return Sequence<Sequence<Actions...>, ActionType>{ sequence, action };
+}
+
+class Eval {};
+static constexpr Eval run{};
+
+template<typename T, typename U, typename = std::enable_if_t<std::is_invocable_v<T> && std::is_invocable_v<U>>>
+auto operator | (const T& lhs, const U& rhs)
+{
+	return Sequence<T>{lhs} | rhs;
+}
+
+auto hello_world() { return []() { std::cout << "hello world" << std::endl; }; }
+
+auto incr(int& i) { return [&i]() { ++i; }; }
+
+template<typename T>
+auto task(const T& invocable)
+{ 
+	return [invocable]() {
+		// queue.submit([=](celerity::handler& cgh) {
+		std::invoke(invocable/*, cgh*/);
+		// }
+	}; 
+}
+
+
+class distr_queue { };
+
+class Runner
+{
+public:
+	Runner(distr_queue q)
+		: queue_(q) { }
+
+
+	void operator()() const {}
+
+private:
+	distr_queue queue_;
+};
+
+Runner submit_to(distr_queue q)
+{
+	return Runner(q);
+}
+
+
