448496a96d255a0b866b5c8a6616ea7f38969d99
Florian Tischler <florian.tischler@outlook.com>
2019-10-28 20:30:57 +0100

    fix get_last_element()

--- src/sequence_traits.h
+++ src/sequence_traits.h
@@ -62,7 +62,7 @@ using last_element_t = typename last_element<T>::type;
 template <typename T, std::enable_if_t<is_sequence_v<T>, int> = 0>
 last_element_t<T> get_last_element(T seq)
 {
-	return std::get<T::num_actions - 1>(seq);
+	return std::get<T::num_actions - 1>(seq.actions());
 }
 
 } // namespace celerity::algorithm
