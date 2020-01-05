689241fa5bac4b950d264b9912edbf8f2acd16c1
Florian Tischler <florian.tischler@outlook.com>
2020-01-05 15:46:59 +0100

    enable trivial copy algorithm

--- src/algorithms/copy.h
+++ src/algorithms/copy.h
@@ -19,7 +19,7 @@ namespace detail
 template <typename ExecutionPolicy, typename IteratorType, typename T, int Rank>
 auto copy(ExecutionPolicy p, buffer_iterator<T, Rank> beg, buffer_iterator<T, Rank> end, IteratorType out)
 {
-	/*using policy_type = strip_queue_t<ExecutionPolicy>;
+	using policy_type = strip_queue_t<ExecutionPolicy>;
 
 	static_assert(!policy_traits<std::decay_t<ExecutionPolicy>>::is_distributed);
 	static_assert(!is_celerity_iterator_v<IteratorType>);
@@ -28,16 +28,16 @@ auto copy(ExecutionPolicy p, buffer_iterator<T, Rank> beg, buffer_iterator<T, Ra
 		auto in_acc = get_access<policy_type, cl::sycl::access::mode::read, one_to_one>(cgh, beg, end);
 
 		if constexpr (algorithm::is_contiguous_iterator<IteratorType>() &&
-					  std::is_same_v<T, typename std::iterator_traits<IteratorType>::value_type>)
+					  sizeof(T) == sizeof(typename std::iterator_traits<IteratorType>::value_type))
 		{
 			return [=]() { memcpy(out, in_acc.get_accessor().get_pointer(), distance(beg, end).size() * sizeof(T)); };
 		}
 		else
 		{
-			auto out_copy = out;
-			return [=](auto item) { *out_copy++ = in_acc[item]; };
+			throw std::runtime_error("not supported");
+			//return [=](auto item) mutable { *out_copy++ = in_acc[item]; };
 		}
-	};*/
+	};
 }
 
 } // namespace detail
