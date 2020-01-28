2f964d5720a59ffa6d23259eef1d3c80171d5a3c
Florian Tischler <florian.tischler@outlook.com>
2020-01-28 12:49:02 +0100

    make any_accessor non-copyable and non-movable weird behavior when copying/moving any_accessors to function parameters use general-purpose apply() to perform actions on stored accessor

--- src/any_accessor.h
+++ src/any_accessor.h
@@ -7,11 +7,23 @@
 namespace celerity::detail
 {
 
-template <typename T>
+template <class T>
+void destruct(T &o)
+{
+    o.~T();
+}
+
+template <class T>
+void placement_copy_construct(void *addr, const T &o)
+{
+    new (addr) T(o);
+}
+
+template <typename T, typename Storage = std::aligned_storage_t<128, 16>>
 class any_accessor
 {
 public:
-    using storage_type = std::aligned_storage_t<128>;
+    using storage_type = Storage;
 
     template <typename AccessorType,
               std::enable_if_t<std::is_same_v<T, accessor_value_type_t<AccessorType>>, int> = 0>
@@ -20,62 +32,167 @@ public:
                                                      target_(accessor_target_v<AccessorType>)
     {
         static_assert(sizeof(AccessorType) <= sizeof(storage_type));
-        new (&storage_) AccessorType(acc);
+        placement_copy_construct(&storage_, acc);
+    }
+
+    /*any_accessor(const any_accessor &that)
+        : rank_(that.rank_),
+          mode_(that.mode_),
+          target_(that.target_)
+    {
+        that.apply([this](auto acc) {
+            placement_copy_construct(&storage_, acc);
+        });
+    }*/
+
+    any_accessor(const any_accessor &that) = delete;
+    any_accessor(any_accessor &&) = delete;
+    any_accessor &operator=(const any_accessor &) = delete;
+    any_accessor &operator=(any_accessor &&) = delete;
+
+    ~any_accessor()
+    {
+        apply([](auto &acc) { destruct(acc); });
     }
 
     template <int Rank>
-    T get(cl::sycl::id<Rank> id) const
+    decltype(auto) get(cl::sycl::id<Rank> id) const
     {
-        return get<Rank>(mode_, target_, id);
+        return apply<Rank>(mode_, target_, [=](auto acc) { return acc[id]; });
     }
 
 private:
-    template <typename AccessorType, int Rank>
-    T get(cl::sycl::id<Rank> id) const
+    template <typename F>
+    decltype(auto) apply(F f) const
     {
-        const auto &acc = (*reinterpret_cast<const AccessorType *>(&storage_));
-        return acc[id];
+        return apply(rank_, mode_, target_, f);
     }
 
-    template <int Rank, cl::sycl::access::mode Mode>
-    T get(cl::sycl::access::target target, cl::sycl::id<Rank> id) const
+    template <typename F>
+    decltype(auto) apply(F f)
+    {
+        return apply(rank_, mode_, target_, f);
+    }
+
+    template <typename AccessorType, typename F>
+    decltype(auto) apply(F f) const
+    {
+        const auto &acc = (*reinterpret_cast<const AccessorType *>(&storage_));
+        return std::invoke(f, acc);
+    }
+
+    template <typename AccessorType, typename F>
+    decltype(auto) apply(F f)
+    {
+        auto &acc = (*reinterpret_cast<AccessorType *>(&storage_));
+        return std::invoke(f, acc);
+    }
+
+    template <int Rank, cl::sycl::access::mode Mode, typename F>
+    decltype(auto) apply(cl::sycl::access::target target, F f) const
     {
         using namespace cl::sycl::access;
 
         switch (target)
         {
         case target::global_buffer:
-            return get<device_accessor<T, Rank, Mode, target::global_buffer>>(id);
+            return apply<device_accessor<T, Rank, Mode, target::global_buffer>>(f);
         case target::host_buffer:
-            return get<host_accessor<T, Rank, Mode>>(id);
-
+            return apply<host_accessor<T, Rank, Mode>>(f);
         default:
             abort();
-            return T{};
         }
     }
 
-    template <int Rank>
-    T get(cl::sycl::access::mode mode, cl::sycl::access::target target, cl::sycl::id<Rank> id) const
+    template <int Rank, cl::sycl::access::mode Mode, typename F>
+    decltype(auto) apply(cl::sycl::access::target target, F f)
+    {
+        using namespace cl::sycl::access;
+
+        switch (target)
+        {
+        case target::global_buffer:
+            return apply<device_accessor<T, Rank, Mode, target::global_buffer>>(f);
+        case target::host_buffer:
+            return apply<host_accessor<T, Rank, Mode>>(f);
+        default:
+            abort();
+        }
+    }
+
+    template <int Rank, typename F>
+    decltype(auto) apply(cl::sycl::access::mode mode, cl::sycl::access::target target, F f) const
     {
         using namespace cl::sycl::access;
 
         switch (mode)
         {
         case mode::read:
-            return get<Rank, mode::read>(target, id);
+            return apply<Rank, mode::read>(target, f);
         case mode::write:
-            return get<Rank, mode::write>(target, id);
+            return apply<Rank, mode::write>(target, f);
         case mode::read_write:
-            return get<Rank, mode::read_write>(target, id);
-
+            return apply<Rank, mode::read_write>(target, f);
         default:
             abort();
-            return T{};
         }
     }
 
-    const int rank_;
+    template <int Rank, typename F>
+    decltype(auto) apply(cl::sycl::access::mode mode, cl::sycl::access::target target, F f)
+    {
+        using namespace cl::sycl::access;
+
+        switch (mode)
+        {
+        case mode::read:
+            return apply<Rank, mode::read>(target, f);
+        case mode::write:
+            return apply<Rank, mode::write>(target, f);
+        case mode::read_write:
+            return apply<Rank, mode::read_write>(target, f);
+        default:
+            abort();
+        }
+    }
+
+    template <typename F>
+    decltype(auto) apply(int rank, cl::sycl::access::mode mode, cl::sycl::access::target target, F f) const
+    {
+        using namespace cl::sycl::access;
+
+        switch (rank)
+        {
+        case 1:
+            return apply<1>(mode, target, f);
+        case 2:
+            return apply<2>(mode, target, f);
+        case 3:
+            return apply<3>(mode, target, f);
+        default:
+            abort();
+        }
+    }
+
+    template <typename F>
+    decltype(auto) apply(int rank, cl::sycl::access::mode mode, cl::sycl::access::target target, F f)
+    {
+        using namespace cl::sycl::access;
+
+        switch (rank)
+        {
+        case 1:
+            return apply<1>(mode, target, f);
+        case 2:
+            return apply<2>(mode, target, f);
+        case 3:
+            return apply<3>(mode, target, f);
+        default:
+            abort();
+        }
+    }
+
+    const long rank_;
     const cl::sycl::access::mode mode_;
     const cl::sycl::access::target target_;
     storage_type storage_;
