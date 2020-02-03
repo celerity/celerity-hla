5dd31881405c79a8be1cba0ac1809e06f080b366
Florian Tischler <florian.tischler@outlook.com>
2020-02-03 15:43:55 +0100

    rename packaged_task traits

--- src/computation_type_traits.h
+++ src/computation_type_traits.h
@@ -12,7 +12,7 @@ namespace celerity::algorithm::detail
 template <typename T>
 constexpr computation_type get_computation_type()
 {
-    if constexpr (detail::is_task_decorator_v<T>)
+    if constexpr (detail::is_packaged_task_v<T>)
     {
         return T::computation_type;
     }
@@ -23,32 +23,32 @@ constexpr computation_type get_computation_type()
 }
 
 template <typename T, computation_type Type>
-struct is_computation_type : std::bool_constant<get_computation_type<T>() == Type>
+struct computation_type_of : std::bool_constant<get_computation_type<T>() == Type>
 {
 };
 
 template <typename T, computation_type Type>
-constexpr inline bool is_computation_type_v = is_computation_type<T, Type>::value;
+constexpr inline bool computation_type_of_v = computation_type_of<T, Type>::value;
 
-template <typename T, std::enable_if_t<detail::is_computation_type_v<T, computation_type::transform>, int> = 0>
+template <typename T, std::enable_if_t<detail::computation_type_of_v<T, computation_type::transform>, int> = 0>
 constexpr access_type get_access_type()
 {
     return T::access_type;
 }
 
-template <typename T, std::enable_if_t<detail::is_computation_type_v<T, computation_type::zip>, int> = 0>
+template <typename T, std::enable_if_t<detail::computation_type_of_v<T, computation_type::zip>, int> = 0>
 constexpr access_type get_first_access_type()
 {
     return T::first_access_type;
 }
 
-template <typename T, std::enable_if_t<detail::is_computation_type_v<T, computation_type::zip>, int> = 0>
+template <typename T, std::enable_if_t<detail::computation_type_of_v<T, computation_type::zip>, int> = 0>
 constexpr access_type get_second_access_type()
 {
     return T::second_access_type;
 }
 
-template <typename T, std::enable_if_t<!detail::is_task_decorator_v<T> || detail::is_computation_type_v<T, computation_type::generate>, int> = 0>
+template <typename T, std::enable_if_t<!detail::is_packaged_task_v<T> || detail::computation_type_of_v<T, computation_type::generate>, int> = 0>
 constexpr access_type get_access_type()
 {
     return access_type::invalid;
