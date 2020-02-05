27f56d110cb2d155d810cd7f9c574341b5c7db0f
Florian Tischler <florian.tischler@outlook.com>
2020-02-05 14:25:46 +0100

    properly implement sequence size trait

--- src/sequence_traits.h
+++ src/sequence_traits.h
@@ -14,8 +14,19 @@ struct sequence_traits : std::integral_constant<bool, false>
 template <typename T>
 constexpr inline bool is_sequence_v = sequence_traits<T>::value;
 
-template <typename T, std::enable_if_t<is_sequence_v<T>, int> = 0>
-constexpr inline auto size_v = T::num_actions;
+template <typename T, bool Sequence = false>
+struct size : std::integral_constant<int, 1>
+{
+};
+
+template <typename T>
+struct size<T, true> : std::integral_constant<int, T::num_actions>
+{
+};
+
+template <typename T>
+constexpr inline auto size_v = size<T, is_sequence_v<T>>::value;
+;
 
 template <typename T>
 struct last_element;
