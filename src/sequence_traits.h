1d81cdc182bd7c50d8f93e99fa97adc518cbb47e
Florian Tischler <florian.tischler@outlook.com>
2020-02-15 16:45:01 +0100

    add trait for accessing first sequence action type

--- src/sequence_traits.h
+++ src/sequence_traits.h
@@ -36,6 +36,12 @@ struct last_element;
 template <typename T>
 using last_element_t = typename last_element<T>::type;
 
+template <typename T>
+struct first_element;
+
+template <typename T>
+using first_element_t = typename first_element<T>::type;
+
 template<typename T>
 struct first_result
 {
