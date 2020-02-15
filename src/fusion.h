bcae2311d7c3fee0fef31ed8e470f1829e076105
Florian Tischler <florian.tischler@outlook.com>
2020-02-15 16:39:49 +0100

    implement t-joints required to support flowing in another sequence into zip

--- src/fusion.h
+++ src/fusion.h
@@ -2,6 +2,7 @@
 #define FUSION_H
 
 #include "celerity_helper.h"
+#include "packaged_task_traits.h"
 
 namespace celerity::algorithm
 {
@@ -152,6 +153,127 @@ bool are_equal(transient_buffer<ElementType, Rank> a, transient_buffer<ElementTy
     return a.get_id() == b.get_id();
 }
 
+template<typename Task, typename SecondaryInputSequence>
+struct t_joint
+{
+public:
+    t_joint(Task task, SecondaryInputSequence sequence)
+        : task_(task), secondary_in_(sequence)
+    {
+
+    }
+
+    auto operator()(celerity::distr_queue& queue) const
+    {
+        std::invoke(secondary_in_, queue);
+        return std::invoke(task_, queue);
+    }
+
+    auto get_in_beg() const { return task_.get_in_beg(); }
+    auto get_in_end() const { return task_.get_in_end(); }
+    auto get_out_iterator() const { return task_.get_out_iterator(); }
+    auto get_range() const { return task_.get_range(); }
+    auto get_task() { return task_.get_task(); }
+
+private:
+    Task task_;
+    SecondaryInputSequence secondary_in_;
+};
+
+template<typename Task, typename SecondaryInputSequence>
+struct partial_t_joint
+{
+public:
+    partial_t_joint(Task task, SecondaryInputSequence sequence)
+        : task_(task), secondary_in_(sequence)
+    {
+
+    }
+
+    template<typename IteratorType>
+    auto complete(IteratorType beg, IteratorType end)
+    {
+        return t_joint{ task_.complete(beg, end), secondary_in_ };
+    }
+
+    auto get_in_beg() const { return task_.get_in_beg(); }
+    auto get_in_end() const { return task_.get_in_end(); }
+    auto get_range() const { return task_.get_range(); }
+
+private:
+    Task task_;
+    SecondaryInputSequence secondary_in_;
+};
+
+namespace detail
+{
+
+template<typename Task, typename SecondaryInputSequence>
+struct is_packaged_task<t_joint<Task, SecondaryInputSequence>>
+    : std::bool_constant<true>
+{
+};
+
+template<typename Task, typename SecondaryInputSequence>
+struct packaged_task_traits<t_joint<Task, SecondaryInputSequence>>
+{
+    using traits = packaged_task_traits<Task>;
+
+    static constexpr auto rank = traits::rank;
+    static constexpr auto computation_type = traits::computation_type;
+    static constexpr auto access_type = traits::access_type;
+
+    using input_iterator_type = typename traits::input_iterator_type;
+    using input_value_type = typename traits::input_value_type;
+    using output_value_type = typename traits::output_value_type;
+    using output_iterator_type = typename traits::output_iterator_type;
+};
+
+template<typename Task, typename SecondaryInputSequence>
+struct extended_packaged_task_traits<t_joint<Task, SecondaryInputSequence>, computation_type::zip> 
+    : extended_packaged_task_traits<Task, computation_type::zip>
+{};
+
+template<typename Task, typename SecondaryInputSequence>
+struct is_partially_packaged_task<partial_t_joint<Task, SecondaryInputSequence>>
+    : std::bool_constant<true>
+{
+};
+
+template<typename Task, typename SecondaryInputSequence>
+struct packaged_task_traits<partial_t_joint<Task, SecondaryInputSequence>>
+{
+    using traits = packaged_task_traits<Task>;
+
+    static constexpr auto rank = traits::rank;
+    static constexpr auto computation_type = traits::computation_type;
+    static constexpr auto access_type = traits::access_type;
+
+    using input_iterator_type = typename traits::input_iterator_type;
+    using input_value_type = typename traits::input_value_type;
+    using output_value_type = typename traits::output_value_type;
+    using output_iterator_type = typename traits::output_iterator_type;
+};
+
+template<typename Task, typename SecondaryInputSequence>
+struct partially_packaged_task_traits<partial_t_joint<Task, SecondaryInputSequence>>
+{
+    using traits = partially_packaged_task_traits<Task>;
+
+    static constexpr auto requirement = traits::requirement;
+};
+
+template<typename T>
+struct is_t_joint : std::bool_constant<false> {};
+
+template<typename Task, typename SecondaryInputSequence>
+struct is_t_joint<t_joint<Task, SecondaryInputSequence>>
+    : std::bool_constant<true> {};
+
+template<typename T>
+constexpr inline bool is_t_joint_v = is_t_joint<T>::value;
+
+}
 
 std::string to_string(access_type type)
 {
@@ -206,7 +328,7 @@ void to_string(std::stringstream& ss, T seq, std::index_sequence<Is...>)
     ((to_string(ss, std::get<Is>(seq.actions()))), ...);
 }
 
-template<typename T, std::enable_if_t<is_sequence_v<T>, int> = 0>
+template<typename T, std::enable_if_t<detail::is_packaged_task_sequence_v<T>, int> = 0>
 std::string to_string(T seq)
 {
     std::stringstream ss{};
