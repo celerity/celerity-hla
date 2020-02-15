c790446a098a905eca2ba31ffb9027c8306ee5a4
Florian Tischler <florian.tischler@outlook.com>
2020-02-15 16:44:25 +0100

    fix traits

--- src/packaged_task.h
+++ src/packaged_task.h
@@ -25,7 +25,8 @@ template <typename T>
 inline constexpr bool single_element_access_v = (detail::computation_type_of_v<T, computation_type::transform> &&
                                                  detail::access_type_v<T> == access_type::one_to_one) ||
                                                 (detail::computation_type_of_v<T, computation_type::zip> &&
-                                                 detail::access_type_v<T> == access_type::one_to_one);
+                                                 detail::access_type_v<T> == access_type::one_to_one && 
+                                                 detail::second_input_access_type_v<T> == access_type::one_to_one);
 
 template <typename T>
 inline constexpr bool is_linkable_source_v = detail::is_partially_packaged_task_v<T> &&
@@ -36,10 +37,10 @@ inline constexpr bool is_linkable_sink_v = detail::is_partially_packaged_task_v<
                                            detail::stage_requirement_v<T> == stage_requirement::input;
 
 template <typename T>
-inline constexpr bool is_transiently_linkable_source_v = is_linkable_source_v<T>; // && (is_source_v<T> || single_element_access_v<T>);
+inline constexpr bool is_transiently_linkable_source_v = is_linkable_source_v<T> && !detail::is_t_joint_v<T>; // && (is_source_v<T> || single_element_access_v<T>);
 
 template <typename T>
-inline constexpr bool is_transiently_linkable_sink_v = is_linkable_sink_v<T> && single_element_access_v<T>;
+inline constexpr bool is_transiently_linkable_sink_v = is_linkable_sink_v<T> && single_element_access_v<T> && !detail::is_t_joint_v<T>;
 
 template <typename T>
 inline constexpr bool has_transient_output_v = is_transient_v<typename detail::packaged_task_traits<T>::output_iterator_type>;
@@ -245,7 +246,7 @@ auto terminate(T seq)
 }
 
 template <typename T, std::enable_if_t<detail::is_partially_packaged_task_v<T> &&
-                                           detail::stage_requirement_v<T> == stage_requirement::output,
+                                       detail::stage_requirement_v<T> == stage_requirement::output,
                                        int> = 0>
 auto operator|(T lhs, distr_queue q)
 {
