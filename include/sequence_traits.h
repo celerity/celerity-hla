86da40417ce24e72fa3792c652e6e2b2dc78bc54
Florian Tischler <florian.tischler@outlook.com>
2020-06-08 22:09:31 +0200

    decay type in sequence element traits

--- include/sequence_traits.h
+++ include/sequence_traits.h
@@ -32,19 +32,19 @@ template <typename T>
 struct last_element;
 
 template <typename T>
-using last_element_t = typename last_element<T>::type;
+using last_element_t = typename last_element<std::decay_t<T>>::type;
 
 template <typename T>
 struct first_element;
 
 template <typename T>
-using first_element_t = typename first_element<T>::type;
+using first_element_t = typename first_element<std::decay_t<T>>::type;
 
 template <size_t Idx, typename T>
 struct nth_element;
 
 template <size_t Idx, typename T>
-using nth_element_t = typename nth_element<Idx, T>::type;
+using nth_element_t = typename nth_element<Idx, std::decay_t<T>>::type;
 
 template <typename T>
 struct first_result
